e52e96fb184632cab65ead2d35193968d29072ee
Mark Kockerbeck <xebxeb@gmail.com>
2008-06-02 07:33:46 -0700

    Tagging System

--- app.py
+++ app.py
@@ -10,34 +10,55 @@ from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
 
-class ListPage:
-    """ A Common Class for Rendering the Main Template """
-    def show(self, handler):
+""" Displays the Main template """
+class Common:
+    """ Displays the Main template """
+    def showMain(self, handler):
         user = users.get_current_user()
-        alltasks = db.GqlQuery( 'SELECT * FROM Task WHERE who = :1 ' 
-                                ' ORDER BY when DESC ', user)
+        alltasks = db.GqlQuery( "SELECT * FROM Task WHERE who = :1 " 
+                                " ORDER BY when DESC ", user)
         values = {
             'user': user,
             'signout' : users.create_logout_url("/"),
             'taglist' : 'None',
             'alltasks' : alltasks,
-			'formaction' : '/app'
+            'formaction' : '/app'
         }
 
-        handler.response.out.write(template.render('templates/app.html', values))   
+        tags = None
+        tagstr = handler.request.get('tags')
+        if tagstr:
+            tags = tagstr[:500].split(',')
+            handler.response.out.write(tags)
 
+        # write out the main template to the given handler
+        handler.response.out.write(template.render('templates/app.html', values))   
+        return
+    
+    def setTags(self, tagString):
+        """ parses a string & returns a list of proper tags (no spaces) """
+        newlist = []
+        for tag in tagString[:500].split(' '):
+            if tag != u' ' and tag != u'':
+                newlist.append(tag.strip())
+        return newlist
+        
+        
 
 class MainHandler(webapp.RequestHandler):
+    
     def get(self):
-        lst = ListPage()
-        lst.show(self)
+        cmn = Common()
+        cmn.showMain(self)
         
     def post(self):
+        cmn = Common()
         task = Task(
             name=self.request.get('taskname')[:200],
-            who=users.get_current_user(),
-			tags=self.request.get('taglist')[:500].split(' ')
-            )
+            who=users.get_current_user())
+        # apply Tags if necessary...
+        if self.request.get('taglist'):
+            task.tags = cmn.setTags(self.request.get('taglist'))
         task.put()
         self.redirect('/app')
 
