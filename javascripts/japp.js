0b8d1b90873b4e0f018a1404d841326c61620bfb
Mark Kockerbeck <xebxeb@gmail.com>
2008-10-27 21:45:03 -0700

    disable() javascript error fixed

--- javascripts/japp.js
+++ javascripts/japp.js
@@ -93,7 +93,7 @@ $(document).ready(function() {
 	                var repeat = false
 	                server.isRepeat(id, function(result) {
 	                    if(result) {
-	                        $('check'+id).attr('enabled', 'false');
+	                        $('#check'+id).attr('disabled', 'disabled');
 	                        repeat = true
 	                    }
 	                    
