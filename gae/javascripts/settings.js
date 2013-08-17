a702bb9fbdb83262ad2627fda94425057259490e
Mark Kockerbeck <mark@kockerbeck.com>
2013-08-17 10:36:09 -0700

    Renaming gae folder to gae-python

--- gae/javascripts/settings.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    hideMsg = function(){ $('h6').hide("slow") }           
-    if($('h6').html() != '') {
-        setTimeout('hideMsg()', 2000);  }
-        
-    $('#nudge').change(function(e){ 
-        setValue('nudge', $(this).val()) }).change();
-});
